de42d606bbabd969ead44120b35efaaf2917eb2a
Virej Dasani <dasanivirej@gmail.com>
2021-01-31 16:14:07 +0530

    Removed unnecessary code

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-// Define board
-let board = {
-    '1': ' -',
-    '2': ' -',
-    '3': ' -',
-    '4': ' -',
-    '5': ' -',
-    '6': ' -',
-    '7': ' -',
-    '8': ' -',
-    '9': ' -'
-}
-
-// This chunk of code is responsible to display the X and O and whose turn it is on the board
-//  --------------------------------------------------------------------------------
 let s1 = document.getElementById('s1')
 let s2 = document.getElementById('s2')
 let s3 = document.getElementById('s3')
@@ -23,18 +8,8 @@ let s7 = document.getElementById('s7')
 let s8 = document.getElementById('s8')
 let s9 = document.getElementById('s9')
 
-s1.innerHTML = board[1]
-s2.innerHTML = board[2]
-s3.innerHTML = board[3]
-s4.innerHTML = board[4]
-s5.innerHTML = board[5]
-s6.innerHTML = board[6]
-s7.innerHTML = board[7]
-s8.innerHTML = board[8]
-s9.innerHTML = board[9]
-
 let displayTurn = document.getElementById('turn')
-//  --------------------------------------------------------------------------------
+
 
 // This just determines whose turn it is. If turn is divisible by 2, it is p1 and vice versa
 var turn = 2
